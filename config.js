a73393035726faf4688b52a47086d738285455d0
Ivette Guadalupe Hernández Pérez <ivette.hernandez@yaxche.mx>
2019-08-06 16:23:54 -0500

    Configuraciones Heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB || "mongodb://localhost:27017/shop",
+  db: process.env.MONGODB_URI || "mongodb://heroku_z71vxqlz:uah7rminmoadf1scheoact5a0n@ds259787.mlab.com:59787/heroku_z71vxqlz",
   SECRET_TOKEN: "tokenkey"
 };
